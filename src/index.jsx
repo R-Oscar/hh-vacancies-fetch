fe507b47cabd4f1cacccbacc1eb93df2d50cf3ba
R-Oscar <askar.payment@gmail.com>
2019-02-06 17:19:43 +0500

    add material-ui

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 
 export default function App() {
-  return <>Hello world! Hmr test</>;
+  return (
+    <MuiThemeProvider>
+      <>
+        <CssBaseline />
+        Hello world! Hmr test
+      </>
+    </MuiThemeProvider>
+  );
 }
 
 render(<App />, document.querySelector('#root'));
